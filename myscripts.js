d316c2929653d03ffbd57902adf5fc0f13d676fa
Davis Ransom <davisransom.vb@gmail.com>
2021-11-25 21:30:05 -0800

    First commit

---
+++ myscripts.js
@@ -0,0 +1,4 @@
+const button = document.querySelector('button');
+
+button.addEventListener("click", function(){ alert("Emily and Davis are having a baby! You are going to be a granddad again!"); });
+
